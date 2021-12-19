f5a731bf4245ff7796b2c607130ac31bce3f96d6
CHIMMILI <muralichimmili@gmail.com>
2021-12-19 11:21:13 +0530

    added new files

--- terraform_mutable/vpc/routes.tf
+++ terraform_mutable/vpc/routes.tf
@@ -2,26 +2,31 @@ resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
   route = [
     {
-    cidr_block = var.cidr_block_default
-    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-    egress_only_gateway_id = ""
-    transit_gateway_id = ""
-    ipv6_cidr_block = ""
-    network_interface_id = ""
-    gateway_id = ""
-    nat_gateway_id = ""
-    instance_id = ""
+      cidr_block                 = var.cidr_block_default
+      vpc_peering_connection_id  = aws_vpc_peering_connection.peering.id
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      gateway_id                 = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      nat_gateway_id             = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
   },
     {
-      cidr_block = "0.0.0.0/0"
-      vpc_peering_connection_id = ""
-      egress_only_gateway_id = ""
-      transit_gateway_id = ""
-      ipv6_cidr_block = ""
-      network_interface_id = ""
-      gateway_id = ""
-      nat_gateway_id = aws_nat_gateway.ngw.id
-      instance_id = ""
+      cidr_block                 = "0.0.0.0/0"
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      gateway_id                 = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      nat_gateway_id             = aws_nat_gateway.ngw.id
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
     }
   ]
   tags = {
@@ -34,26 +39,32 @@ resource "aws_route_table" "public_rt" {
   route  = [
 
     {
-      cidr_block = var.cidr_block_default
-      vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-      egress_only_gateway_id = ""
-      transit_gateway_id = ""
-      ipv6_cidr_block = ""
-      network_interface_id = ""
-      gateway_id = ""
-      nat_gateway_id = ""
-      instance_id = ""
+      cidr_block                 = var.cidr_block_default
+      vpc_peering_connection_id  = aws_vpc_peering_connection.peering.id
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      gateway_id                 = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      nat_gateway_id             = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
     },
     {
-      cidr_block = "0.0.0.0/0"
-      vpc_peering_connection_id = ""
-      egress_only_gateway_id = ""
-      transit_gateway_id = ""
-      ipv6_cidr_block = ""
-      network_interface_id = ""
-      gateway_id =aws_internet_gateway.igw.id
-      nat_gateway_id = ""
-      instance_id = ""
+      cidr_block                 = "0.0.0.0/0"
+      vpc_peering_connection_id  = ""
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      gateway_id                 = aws_internet_gateway.igw.id
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      nat_gateway_id             = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
     }
 
 ]
