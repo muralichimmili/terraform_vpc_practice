0c3cf7d5a3045d00d0d822ab33695b886b43247a
CHIMMILI <muralichimmili@gmail.com>
2021-12-19 12:27:55 +0530

    added new files

--- terraform_mutable/vpc/vars.tf
+++ terraform_mutable/vpc/vars.tf
@@ -6,5 +6,5 @@ variable "PUBLIC_SUBNETS" {}
 variable "AZS" {}
 variable "DEFAULT_VPC_ID" {}
 variable "DEFAULT_VPC_CIDR" {}
-variable "INTERNAL_HOSTEDZONE_ID" {}
-variable "PUBLIC_HOSTEDZONE_ID" {}
+#variable "INTERNAL_HOSTEDZONE_ID" {}
+#variable "PUBLIC_HOSTEDZONE_ID" {}
